ec2bb63eed8add3e190cd1da709f58b5dd9b36d8
unknown <kaushikyash262@gmail.com>
2023-09-20 18:18:31 +0530

    Authentication Added

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -1,43 +1,129 @@
-import { VStack, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import {
+  VStack,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
 
   const handleClick = () => {
     setShow(!show);
-  }
+  };
 
-  const handleSubmit = () => {}
+  const handleSubmit = async () => {
+    setLoading(true);
+
+    if (!email || !password) {
+      toast({
+        title: "Please fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+      toast({
+        title: "Login Successfull!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "An Error Occured! Please try again.",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+    }
+  };
 
   return (
-    <VStack spacing='5px' color='black'>
-        <FormControl id='email' isRequired>
-            <FormLabel>Email</FormLabel>
-            <Input placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} />
-        </FormControl>
+    <VStack spacing="5px" color="black">
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          value={email}
+          placeholder="Enter Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
 
-        <FormControl id='password' isRequired>
-            <FormLabel>Password</FormLabel>
-            <InputGroup>
-               <Input type={show ? 'text' : 'password'} placeholder='Enter Your password' onChange={(e) => setPassword(e.target.value)} />
-               <InputRightElement width='4.5rem'>
-                <Button h='1.75rem' size='sm' onClick={handleClick}>
-                    {show ? "Hide" : "Show"}
-                </Button>
-               </InputRightElement>
-            </InputGroup>
-        </FormControl> 
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            value={password}
+            type={show ? "text" : "password"}
+            placeholder="Enter Your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
 
-        <Button colorScheme='blue' width='100%' style={{ marginTop: 15 }} onClick={handleSubmit} >Login</Button>
-        <Button variant='solid' colorScheme='red' width='100%' onClick={() => {
-            setEmail("guestaccount@gmail.com");
-            setPassword("123456");
-        }} >Get Guest User Credentials</Button>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={handleSubmit}
+        isLoading={loading}
+      >
+        Login
+      </Button>
+      <Button
+        variant="solid"
+        colorScheme="red"
+        width="100%"
+        onClick={() => {
+          setEmail("guestaccount@gmail.com");
+          setPassword("123456");
+        }}
+      >
+        Get Guest User Credentials
+      </Button>
     </VStack>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
